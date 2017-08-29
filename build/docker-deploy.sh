6431a447136de359397a996cf357c6b42f0b4f9f
somax <somaxj@163.com>
2017-08-29 20:40:12 +0800

    更新 docker 部署脚本，先尝试移除运行中的容器

--- build/docker-deploy.sh
+++ build/docker-deploy.sh
@@ -6,11 +6,13 @@ VERSION=$3
 # 清屏
 echo -e "\033[2J"
 
-echo "Copy '$DOCKER_IMAGE' to $REMOTEHOST"
+echo "Uploading '$DOCKER_IMAGE' to $REMOTEHOST"
 scp "$DOCKER_IMAGE_PATH$DOCKER_IMAGE" "$REMOTEHOST:~/" &&
-echo "Loading docker image..." &&
+echo "Loading docker image from $DOCKER_IMAGE..." &&
 ssh -t $REMOTEHOST "
     docker image load < $DOCKER_IMAGE &&
+    echo 'Try to remove running containner...' &&
+    docker stop laputa-app
     docker run --rm --name laputa-app -p 18080:80 -d laputa-app:$VERSION" && 
     echo "$(date) \033[32mDocker deploy success. \033[0m" || echo "$(date) \007 \033[31mDocker deploy failed! \033[0m" 
 
