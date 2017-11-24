6a43e3c4bc1952270c0e908fa7f05f900182ad32
chaihong <chaihong@jkinfo.com.cn>
2017-11-24 11:49:42 +0800

    重构图表的渲染逻辑

--- src/components/Ble/echarts.vue
+++ src/components/Ble/echarts.vue
@@ -9,13 +9,12 @@
       </q-toolbar-title>
     </div>
     <div class="layout-view">
-      <div class="layout-padding chart-padding" v-if='echartsArray.length'  >
+      <div class="layout-padding chart-padding" v-if='echartCrt' >
         <p class="caption monitor-title"> {{echartCrt.name}} </p>
-
-        <div class="echarts" v-for='(chart, index) in echartsArray' :key="index" >
-          <IEcharts :option="chart" :loading="loading"></IEcharts>
-        
-          <!--<IEcharts :option="bar2" :theme="theme" @ready="onReady2"></IEcharts>-->
+        <div class="echarts" v-for='(value, id, index) in echartMonitorData' :key="index" @click="resetEchartData(id)">
+            <IEcharts :option="value.chart" :loading="value.loading" @ready="getEchartData(id)"></IEcharts>  
+          <!--     
+        <IEcharts :option="bar2" :theme="theme"></IEcharts>-->
         </div>
       </div>
     </div>
@@ -23,6 +22,15 @@
 </template>
 
 <script type="text/babel">
+  let mockData={
+    values:[],
+    z:{
+      data:[]
+    },
+    x:{
+      data:[]
+    }
+  };
 
   import IEcharts from 'vue-echarts-v3/src/full.vue'
   import dark from 'echarts/theme/dark'
@@ -37,38 +45,38 @@
   export default {
     name: 'view',
     data(){
-        return{
-          theme: 'dark',
-          title: '历史曲线',
-          loading: true,
-          envChartGap:10,
-          echartsArray:[],
-          echartOpt:{
-            title: {},
-            grid:{
-              top:40,
-              left:40,
-              bottom: 30,
-            },
-            xAxis: {
-              type:'category',
-              axisLabel:{
-                formatter(value,index){
-                   var date = moment(parseInt(value)).format('H:mm')
-                    return date;
-                }
-              },
-            },
-            yAxis: {},
-            series: []
+      return{
+        theme: 'dark',
+        title: '历史曲线',
+        envChartGap:10,
+        echartMonitorData:{}, 
+        echartOpt:{
+          title: {},
+          grid:{
+            top:40,
+            left:40,
+            bottom: 30,
           },
-          echartConf:{
-           'default' : { 
-             name: 'Sales',
-              type: 'line',
-              smooth: true,
-              data:[]}
+          xAxis: {
+            type:'category',
+            axisLabel:{
+              formatter(value,index){
+                  var date = moment(parseInt(value)).format('H:mm')
+                  return date;
+              }
+            },
           },
+          yAxis: {},
+          series: []
+        },
+        echartConf:{
+          'default' : { 
+            name: 'Sales',
+            type: 'line',
+            smooth: true,
+            data:[]
+          }
+        },
       }
     },
     computed:{
@@ -79,75 +87,69 @@
     mounted(){
     },
     watch:{
-      echartCrt(val,oldVal){
+      echartCrt(val, oldVal){
         if(typeof val=='object'){
-          this.getEchartData()
+          for(let i in val.monitors){ 
+             this.echartMonitorData[i]={'loading':true,'chart': {series:[]} } 
+          }
         }
       }
     },
     methods: {
+      getEchartData(monitorId ){ 
+        const that = this
+        let conf = that.echartConf, opt = that.echartOpt; 
+        let monitor= that.echartCrt.monitors[monitorId]
+        let monitorTitle=monitor.name+`(${monitor.value + monitor.unit})`;
+        
+        let chart = {}, monitorChart={};
+        monitorChart[monitorId]={
+          loading:false,
+          chart:{series:[]}
+        }
+        that.find({
+          query: {
+            deviceId: that.echartCrt.id ,
+              monitorId: monitorId,
+              gap:that.envChartGap
+            }
+          }).then(data =>{
+              opt.title.text=monitorTitle
+              chart = echarts.xparse(opt, data[0], conf)
+               monitorChart[monitorId].chart=chart
+               // !!!! vue 对象更改检测注意事项
+              //   https://cn.vuejs.org/v2/guide/list.html#对象更改检测注意事项
+              that.echartMonitorData = Object.assign({}, that.echartMonitorData, monitorChart)
+            })  
+            .catch(e =>{
+              //TODO error 返回空数组 
+              opt.title.text=monitorTitle
+              chart = echarts.xparse(opt, mockData, conf);  
+               monitorChart[monitorId].chart=chart
+              that.echartMonitorData = Object.assign({}, that.echartMonitorData, monitorChart)
+            })   
+       // console.log('-=--=', monitorId, that.echartMonitorData)
+      },
+      resetEchartData(id){
+        //重新请求图表数据
+        let initData ={}
+        initData[id]= {'loading':true,'chart': {series:[]} } 
+        this.echartMonitorData = Object.assign({}, this.echartMonitorData, initData)
+       this.getEchartData(id)
+      },
       ...mapActions('environment_chart', {
         find: 'find',
       }),
-      ...mapActions('environment_chart', {
-        get: 'get',
-      }),
       ...mapMutations('environment_chart', {
-        clear: 'clearAll',
+        clearChartList: 'clearAll',
       }),
-      getEchartData(){
-        let vm =this
-        let deviceId=vm.echartCrt.id
-        let mtrId=Object.keys(vm.echartCrt.monitors)
-        let conf = vm.echartConf, opt = vm.echartOpt;
-        for(let i in mtrId){
-          (function(id){
-           // opt.title.text=vm.echartTitleConf[id]
-            let mnt= vm.echartCrt.monitors[id]
-            let txt=mnt.name+`(${mnt.value + mnt.unit})`
-            vm.find({
-              query: {
-                deviceId: deviceId,
-                  monitorId: id,
-                  gap:vm.envChartGap
-                }
-              }).then(data =>{
-                vm.loading=false
-                 opt.title.text=txt
-                  vm.echartsArray.push(echarts.xparse(opt, data[0] , conf));
-                 //  console.log('--qq---', opt.title)
-                })  
-                .catch(e =>{
-                  //TODO error 返回空数组
-                  vm.loading=false
-                    let data={
-                      values:[],
-                      z:{
-                        data:[]
-                      },
-                      x:{
-                        data:[]
-                      }
-                    }
-                   opt.title.text=txt
-                   vm.echartsArray.push(echarts.xparse(opt, data , conf));
-               /*    if(JSON.stringify(e)=='{}'){
-                  }else{
-                    vm.echartsArray.push(echarts.error)
-                  } */
-                  console.log("--=Oops, error=-=", JSON.stringify(e), e)
-                }) 
-          })(mtrId[i])
-        }
-      },
-      onClick(event, instance, echarts) {
-      }
     },
     components: {
       IEcharts
     },
     destroyed(){
-      this.clear()
+      //清空图表接口返回List数据
+      this.clearChartList()
     }
   }
 </script>
