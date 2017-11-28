162fa8e3a3bcca58e384668724cd8bf10be801c6
chaihong <chaihong@jkinfo.com.cn>
2017-11-28 10:25:24 +0800

    删除多余的文件

--- src/components/conf/storage.js
+++
@@ -1,5 +0,0 @@
-const  filtersStorage=(obj, action) => {
-     let _storage = action?localStorage.setItem(obj.key, JSON.stringify(obj.value)): JSON.parse(localStorage.getItem(obj)) 
-    return _storage;
- }
- export default filtersStorage
